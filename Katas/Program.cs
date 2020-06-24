2599484a13bc4e0b92096494db75309984ee7bc8
NicollahSekete <nicollahtsekete@gmail.com>
2020-06-24 12:23:46 +0100

    * .gitignore: git commit -m "first commit" git remote add origin   https://github.com/NicollahSekete/visual-studio-kata-csharp.git git push -u origin master
    * Katas.sln:
    * Program.cs:
    * pieKata.cs:
    * Katas.csproj:

---
+++ Katas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            pieTime findPie = new pieTime();
+            findPie.pieUp(4, 6);
+        }
+    }
+}
---
+++ Katas/pieKata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Katas
+{
+    class pieTime
+    {
+        public void pieUp(int pieEaters, int slicesEaten)
+        {
+            int workOut = pieEaters % slicesEaten;
+            if (workOut == 0)
+                Console.WriteLine("enough for everyone");
+            else
+                Console.WriteLine("not enough slices sorry");
+
+        }
+    }
+}
